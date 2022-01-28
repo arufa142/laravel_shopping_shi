7813e9fb1234237c7b85ab8b09f9ceb2c8be9105
arufa142 <200084zs@yse-c.net>
2022-01-28 13:54:42 +0900

    うまくできませんで来た

--- resources/views/admin/item/edit.blade.php
+++ resources/views/admin/item/edit.blade.php
@@ -2,19 +2,19 @@
 <a href="{{route('admin.item.index')}}">{{ __('Back') }}</a>
 <form action="{{route('admin.item.update',$item->id)}}"method="post">
 @csrf
-<div>
+<div class="form-floating mb-3">
     <label for="">{{ __('label.item_name') }}</label>
     <input type="text" name="name"value="{{$item->name}}">
   </div>
-  <div>
+  <div class="form-floating mb-3">
     <label for="">{{ __('label.item_code') }}</label>
     <input type="text" name="code"value="{{$item->code}}">
   </div>
-  <div>
+  <div class="form-floating mb-3">
     <label for="">{{ __('label.price') }}</label>
     <input type="number" name="price"value="{{$item->price}}">
   </div>
-  <div>
+  <div class="form-floating mb-3">
     <label for="">{{ __('label.stock') }}</label>
     <input type="number" name="stock"value="{{$item->stock}}">
   </div>
--- resources/views/admin/item/index.blade.php
+++ resources/views/admin/item/index.blade.php
@@ -1,3 +1,6 @@
+@extends('layouts.app')
+@section('content')
+
 <h2>商品一覧</h2>
 <a href="{{route('admin.item.create')}}">商品追加</a>
 
